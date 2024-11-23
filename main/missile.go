9093535c4886a468a69a8297be6bda3fe655e18a
CodeGen2309 <gosha31rus.14@gmail.com>
2024-11-23 04:35:19 +0300

    init commit

---
+++ main/missile.go
@@ -0,0 +1,44 @@
+components {
+  id: "missile"
+  component: "/scripts/missile.script"
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"missile\"\n"
+  "mask: \"enemy\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 16.963451\n"
+  "  data: 28.42107\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Missile02A\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/missiles.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 0.131783
+    y: 0.123077
+  }
+}
